fd3ec9c11544202e88f67e373e740abc4533b0a0
Eder Diego de Sousa <me@diego.dev.br>
2022-12-05 19:49:32 -0300

    Initial commit

---
+++ src/main/java/br/dev/diego/apiparkingcontrol/ApiParkingControlApplication.java
@@ -0,0 +1,13 @@
+package br.dev.diego.apiparkingcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiParkingControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiParkingControlApplication.class, args);
+	}
+
+}
